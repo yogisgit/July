1ffa879ac560667cf73db39694037fbff787067d
yogisgit <yogeshravi2704@gmail.com>
2022-07-04 20:48:15 +0530

    complete framework

--- src/test/java/com/july/tests/RSPracticeTest.java
+++ src/test/java/com/july/tests/RSPracticeTest.java
@@ -3,6 +3,7 @@ package com.july.tests;
 import org.testng.annotations.Test;
 import java.io.IOException;
 
+import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
@@ -15,20 +16,27 @@ import com.july.util.ExcelUtil;
 
 public class RSPracticeTest extends BaseClass{
 	public WebDriver driver;
+	Logger log = Logger.getLogger(RSPracticeTest.class);
+	
 	@BeforeTest
 	public void setUp() throws IOException {
 		driver = initDriver();
+		log.info("Browser opened");
 	}
 	@Test(dataProvider="getData")
 	public void RSAPracticePage(String data) {
 		PracticePageObjects pp = new PracticePageObjects(driver);
 		pp.alert().click();
+		log.info("Tap on alert");
 		
 		System.out.println(driver.switchTo().alert().getText());
 		driver.switchTo().alert().accept();
+		log.info("Accepted alert");
 		
 		pp.alertWithName().sendKeys(data);
+		log.info("Given name to alert");
 		pp.alert().click();
+		log.info("Tap on alert");
 		
 		System.out.println(driver.switchTo().alert().getText());
 		driver.switchTo().alert().accept();
